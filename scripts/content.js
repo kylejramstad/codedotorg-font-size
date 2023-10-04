93871ab383ef745549943b970e99e41cd5d059ca
Kyle Ramstad <kylejramstad@gmail.com>
2023-10-04 15:15:21 -0700

    save font size between use

--- scripts/content.js
+++ scripts/content.js
@@ -2,22 +2,30 @@ const checkingEditor = setInterval(getElement, 500);
 var element;
 var size = 15;
 
+//find app labs code
 function getElement(){
     const editor = document.getElementsByClassName("droplet-ace ace_editor ace-chrome");
     element = editor[0];
     if(element){
         clearInterval(checkingEditor);
+        //load saved Size if it exists
+        chrome.storage.local.get(["savedSize"], get);
     }
 }
 
+function get(items){
+    if(typeof items.savedSize !== undefined){
+        element.style.fontSize = items.savedSize+"px";
+    }
+}
+
+//listen to messages from popup
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(element){
             if(request.type == "set"){
                 size = request.value;
                 element.style.fontSize = size+"px";
-            }else if(request.type == "get"){
-                size = Number(element.style.fontSize);
             }
             sendResponse({size: size});
         }else{
--- scripts/popup.js
+++ scripts/popup.js
@@ -3,15 +3,28 @@ async function setSize(size){
     const response = await chrome.tabs.sendMessage(tab.id, {type:"set", value:size});
     return response.size;
 }
-async function getSize(){
+async function check(){
     const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
     const response = await chrome.tabs.sendMessage(tab.id, {type:"none"});
-    return response.size;
 }
 (async () => {
+    //used to check if code.org app lab
     try{
-        document.getElementById("sizeReport").value = await getSize();
+        await check(); //checking to see if connected to app lab
+
+        //load savedSize
+        chrome.storage.local.get(["savedSize"], async function(items){
+            var size;
+            if(typeof items.savedSize !== undefined){
+                size = items.savedSize;
+            }else{
+                size = 15;
+            }
+            document.getElementById("sizeReport").value = await setSize(size);
+        });
+        
     }catch(e){
+        //display error that it isn't app lab
         document.getElementById("sizeReport").style.display = "none";
         document.getElementById("error").style.display = "inline";
     }
@@ -19,5 +32,7 @@ async function getSize(){
 
 document.getElementById("sizeReport").oninput =
     async function() {
-        await setSize(document.getElementById("sizeReport").value);
+        chrome.storage.local.set({ "savedSize": document.getElementById("sizeReport").value},async function(){
+            await setSize(document.getElementById("sizeReport").value);
+        }); 
     };
